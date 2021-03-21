075ae4527210adf25a15405fc7fee8e0cde361ba
gekal <liu.hongying@hotmail.com>
2021-03-21 18:02:22 +0900

    init commit

---
+++ src/main/java/cn/gekal/sample/confirmconfigurationpriority/ConfirmConfigurationPriorityApplication.java
@@ -0,0 +1,13 @@
+package cn.gekal.sample.confirmconfigurationpriority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfirmConfigurationPriorityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfirmConfigurationPriorityApplication.class, args);
+	}
+
+}
